6ef66d0d0a6885a03df166dce30a4627f0ecde9f
soufianeelotmani <souf2001elotmani@gmail.com>
2025-03-02 16:08:40 +0000

    model update

--- src/main/java/com/model/Room.java
+++ src/main/java/com/model/Room.java
@@ -6,7 +6,6 @@ public class Room {
     private String status;
     private String description;
 
-    // Getters and Setters
     public int getId() { return id; }
     public void setId(int id) { this.id = id; }
 
